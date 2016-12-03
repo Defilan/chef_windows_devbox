57ce41e187cb701975763dcac3c9a9c8f3a213ac
Christopher Maher <defilan@gmali.com>
2016-12-03 12:02:08 -0800

    Initial push

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_windows_devbox
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef_windows_devbox
+# Recipe:: default
+#
+# Copyright (c) 2016 Chris Maher, All Rights Reserved.
